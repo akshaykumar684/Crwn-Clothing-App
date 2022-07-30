ad17aab1ed5a07a716f187c56fba32839689c5ae
Akshay Kumar <akshaykrkat@gmail.com>
2022-07-30 17:30:14 +0530

    created directory compoennt for root of category item

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,5 +1,4 @@
-import "./categories.style.scss";
-import CategoryItem from "./category-item/category-item.component";
+import Directory from "./directory/directory.component";
 const App = () => {
   const categories = [
     {
@@ -28,13 +27,7 @@ const App = () => {
       imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
     },
   ];
-  return (
-    <div className="categories-container">
-      {categories.map((category) => (
-        <CategoryItem key={category.id} category={category} />
-      ))}
-    </div>
-  );
+  return <Directory categories={categories} />;
 };
 
 export default App;
