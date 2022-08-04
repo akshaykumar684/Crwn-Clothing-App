aa8c5cedc28918cf3da47042647e22272f91484e
Akshay Kumar <akshaykrkat@gmail.com>
2022-08-04 12:08:40 +0530

    added Sign Up component

---
+++ crwn-clothing/src/components/sign-up-form/sign-up-form.component.jsx
@@ -0,0 +1,65 @@
+import { useState } from "react";
+
+const defaultFormFields = {
+  displayName: "",
+  email: "",
+  password: "",
+  confirmPassword: "",
+};
+
+const SignUpForm = () => {
+  const [formFields, setFormFields] = useState(defaultFormFields);
+  const { displayName, email, password, confirmPassword } = formFields;
+  const signUpFormSubmitHandler = (e) => {
+    e.preventDefault();
+    console.log("Form Submitted");
+    console.log(formFields);
+  };
+
+  const formInputChangeHandler = (event) => {
+    const { name, value } = event.target;
+    setFormFields({ ...formFields, [name]: value });
+  };
+  return (
+    <div>
+      <h1>Sign up with your email and password</h1>
+      <form onSubmit={signUpFormSubmitHandler}>
+        <label>Display Name</label>
+        <input
+          type="text"
+          value={displayName}
+          name="displayName"
+          onChange={formInputChangeHandler}
+          required
+        />
+        <label>Email</label>
+        <input
+          type="email"
+          value={email}
+          name="email"
+          onChange={formInputChangeHandler}
+          required
+        />
+        <label>Password</label>
+        <input
+          type="password"
+          value={password}
+          name="password"
+          onChange={formInputChangeHandler}
+          required
+        />
+        <label>Confirm Password</label>
+        <input
+          type="password"
+          value={confirmPassword}
+          name="confirmPassword"
+          onChange={formInputChangeHandler}
+          required
+        />
+        <button type="submit">Sign Up</button>
+      </form>
+    </div>
+  );
+};
+
+export default SignUpForm;
